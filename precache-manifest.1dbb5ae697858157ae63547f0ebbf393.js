3b552df3dd32af984832e6e520f1064cbc4c4c58
jsaari97 <jsaari97@users.noreply.github.com>
2020-06-08 23:37:28 +0000

    [skip ci] Updates

---
+++ precache-manifest.1dbb5ae697858157ae63547f0ebbf393.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf54aa4382be6fdba9e7808157826b1",
+    "url": "/musalink/index.html"
+  },
+  {
+    "revision": "e2f9a009674b423a63ec",
+    "url": "/musalink/static/js/2.fb444282.chunk.js"
+  },
+  {
+    "revision": "d6e2ee81fb5475724387e4ca643167c8",
+    "url": "/musalink/static/js/2.fb444282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33411c9f520f7f392527",
+    "url": "/musalink/static/js/main.d37d1247.chunk.js"
+  },
+  {
+    "revision": "7cb050e92c8c747715ee",
+    "url": "/musalink/static/js/runtime-main.c50f66d9.js"
+  },
+  {
+    "revision": "f783f12921a2cf5aace94a0a0f133107",
+    "url": "/musalink/static/media/close.f783f129.svg"
+  },
+  {
+    "revision": "7fa87351dce804f27280e81cc9faf95f",
+    "url": "/musalink/static/media/deezer.7fa87351.svg"
+  },
+  {
+    "revision": "1bae49a499f68120fb6ba894d2a15e81",
+    "url": "/musalink/static/media/pause.1bae49a4.svg"
+  },
+  {
+    "revision": "4b2e91e8aa3f9b0aa5c74aa725e5d346",
+    "url": "/musalink/static/media/play.4b2e91e8.svg"
+  },
+  {
+    "revision": "3e2ba66d1517bbc97140e40aab30f398",
+    "url": "/musalink/static/media/spotify.3e2ba66d.svg"
+  }
+]);
