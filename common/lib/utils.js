0373434f4d8d7954c5a73c3501acfd150df2c970
Jim Saari <jim.saari97@gmail.com>
2021-03-13 09:52:22 +0200

    update project dependencies

--- common/lib/utils.js
+++ common/lib/utils.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.determineService = void 0;
-exports.determineService = (url) => url.match(/open\.spotify/)
+const determineService = (url) => url.match(/open\.spotify/)
     ? "spotify"
     : url.match(/deezer\.com/)
         ? "deezer"
         : null;
+exports.determineService = determineService;
