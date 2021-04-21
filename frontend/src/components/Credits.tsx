b59d8b5caa1b3c1a67e7b334336ac17f6c555c14
Jim Saari <jim.saari97@gmail.com>
2021-04-21 11:34:32 +0300

    Make footer year dynamic

--- frontend/src/components/Credits.tsx
+++ frontend/src/components/Credits.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 
 export const Credits: React.FC = () => {
+  const year = React.useMemo(() => new Date().getFullYear(), []);
+
   return (
     <footer className="absolute bottom-3 left-1/2 transform -translate-x-1/2">
       <p
         className="text-sm text-gray-100"
         style={{ textShadow: "0 2px 4px rgba(0,0,0,0.25)" }}
       >
-        2020 - Made by Jim Saari
+        {year} - Made by Jim Saari
       </p>
     </footer>
   );
